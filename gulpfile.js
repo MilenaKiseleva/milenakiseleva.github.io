1f50d562d05f27f44f2db4ea758c95575dee8e0e
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-04-04 00:17:21 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,99 @@
     const
 
         // Modules
-        { parallel } = require('gulp'),
-        del = require('del');
+        { src, dest, series, parallel } = require('gulp'),
+        del       = require('del'),
+        concat    = require('gulp-concat'),
+        uglify    = require('gulp-uglify'),
+        postcss   = require('gulp-postcss'),
+        babel     = require('gulp-babel'),
+        unprefix  = require("postcss-unprefix"),
+        autoprefixer = require('autoprefixer'),
+        cssnano   = require('cssnano'),
+        htmlmin   = require('gulp-htmlmin'),
+        prettify  = require('gulp-html-prettify'),
+        beautify  = require('gulp-beautify'),
+
+        // Directory locations
+        path = {
+            source  : '.',
+            build   : '.'
+        };
 
     (async (done) => {
         await del(['**/.DS_Store'], done);
     })();
 
-    function execution_time (done) {
-        let timeStart = process.hrtime();
-
-        process.on('exit', (code) => {
-            let timeEnd = process.hrtime(timeStart);
-
-            console.info('Execution time: %ds %dms', timeEnd[0], timeEnd[1] / 1000000);
-        });
-
-        done();
+    function html_optimize () {
+        let files = [
+            `${path.build}/**/*.html`,
+            '!node_modules/**'
+        ];
+        let htmlmin_opts = {
+            html5: true,                         // Parse input according to HTML5 specifications
+            preserveLineBreaks: true,            // Always collapse to 1 line break (never remove it entirely) when whitespace between tags include a line break. Must be used in conjunction with collapseWhitespace=true
+            collapseWhitespace: true,            // Collapse white space that contributes to text nodes in a document tree
+            collapseBooleanAttributes: true,     // Omit attribute values from boolean attributes
+            //removeComments: true,                // Strip HTML comments
+            //removeEmptyElements: true,           // Remove all elements with empty contents
+            removeEmptyAttributes: true,         // Remove all attributes with whitespace-only values
+            removeScriptTypeAttributes: true,    // Remove type="text/javascript" from script tags. Other type attribute values are left intact
+            removeStyleLinkTypeAttributes: true, // Remove type="text/css" from style and link tags. Other type attribute values are left intact
+            //minifyJS: true,                      // Minify JavaScript in script elements and event attributes (uses UglifyJS)
+            //minifyCSS: true                      // Minify CSS in style elements and style attributes (uses clean-css)
+        };
+        let beautify_opts = {
+            indent_size: 4
+        };
+        let prettify_opts = {
+            indent_char: ' ',
+            indent_size: 4
+        };
+        return src(files)
+            .pipe(htmlmin(htmlmin_opts))
+            .pipe(prettify(prettify_opts))
+            .pipe(beautify.html(beautify_opts))
+            .pipe(dest(`${path.build}/`));
     }
 
-    function default_function (done) {
+    function stylesheets () {
+        let css_files = [
+            `${path.build}/css/*.css`
+        ];
+        let cssnano_opts = {
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true,
+                },
+            }]
+        };
+        return src(css_files)
+            .pipe(postcss([
+                unprefix(),
+                autoprefixer(),
+                cssnano(cssnano_opts)
+            ]))
+            .pipe(dest(`${path.build}/css/`));
+    }
 
-        // Code here
-
-        done();
+    function javascript_bundle () {
+        let js_files = [
+            `${path.source}/scripts/*.js`,
+            `!${path.source}/scripts/scripts.js`
+        ];
+        return src(js_files)
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(concat('scripts.js'))
+            .pipe(uglify())
+            .pipe(dest(`${path.build}/scripts/`));
     }
 
     exports.html_optimize = html_optimize;
-    exports.default = parallel(execution_time, default_function);
+    exports.html = html_optimize;
+    exports.css = stylesheets;
+    exports.js = javascript_bundle;
+    exports.default = parallel(html_optimize, stylesheets, javascript_bundle);
 
 })();
